d721c5990b61b34fdb2e5792269e7e6fa57f765c
ajk0421 <ajk.spirituallounge@gmai.com>
2021-11-23 23:41:20 +0900

    setting application.rb

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,13 @@ module WonderfulMemoApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.jbuilder false
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
