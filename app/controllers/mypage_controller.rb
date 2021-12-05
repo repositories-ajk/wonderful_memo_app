084c7dad72644cc5a9cd46e0543fa1a923c25801
ajk0421 <ajk.spirituallounge@gmai.com>
2021-12-05 12:34:01 +0900

    mypage検索

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,6 +1,12 @@
 class MypageController < ApplicationController
   # GET /mypage
   def index
-    @memos = current_user.memos.page params[:page]
+    memos = current_user.memos
+
+    if params[:title].present?
+      memos = memos.where("title LIKE ?", "%#{params[:title]}%")
+    end
+
+    @memos = memos.page params[:page]
   end
 end
