2054b67991730a83e7a18e9a4704abfae2a52748
ajk0421 <ajk.spirituallounge@gmai.com>
2021-12-05 18:23:32 +0900

    seed追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,7 @@ tags.each { |tag| Tag.find_or_create_by!(name: tag) }
     ii += 1
     user.articles.find_or_create_by!(title: "No.#{ii}: user00#{i}の記事") do |article|
       article.content = "No.#{ii}: user00#{i}の記事の本文"
+      article.tag_ids = Tag.all.pluck(:id)
     end
   end
 end
